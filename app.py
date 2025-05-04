717df6e812be7a67d5b715139cada72bdf8d3d92
Ali Habib <alihabib.5714@gmail.com>
2025-05-04 14:25:00 +0500

    added routes

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")  # base route
 def home():
-    return "<p>This is my home page!</p>"
+    return render_template("home.html")
+
+
+@app.route("/add")  # adds a new contact
+def add_contact():
+    return render_template("add.html")
+
+
+@app.route("/read")  # display all contacts
+def read_contacts():
+    return render_template("read.html")
+
+
+@app.route("/update")  # updates an existing contact
+def update_contact():
+    return render_template("update.html")
+
+
+@app.route("/delete")  # deletes an existing contact
+def delete_contact():
+    return render_template("delete.html")
 
 
 if __name__ == "__main__":
