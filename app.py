2d800c4cedfb1040961d64a1bb2e2fe2d21f9f2a
Ali Habib <alihabib.5714@gmail.com>
2025-05-05 21:41:32 +0500

    added read feature

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+contacts = [
+    {"name": "Ali Habib", "no": "03313568393"},
+    {"name": "Maryam Hayat", "no": "03415947090"},
+    {"name": "Habib Hussain", "no": "03212727914"},
+]
+
+# contacts = []
+
 
 @app.route("/")
 @app.route("/home")  # base route
@@ -16,7 +24,7 @@ def add_contact():
 
 @app.route("/read")  # display all contacts
 def read_contacts():
-    return render_template("read.html")
+    return render_template("read.html", contacts=contacts)
 
 
 @app.route("/update")  # updates an existing contact
